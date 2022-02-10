fc4124ba0d2d939489e8ca51bb12afbfc5e143f0
Marcel Wiessler <marcel@gaisterhand.de>
2022-02-10 17:43:16 +0100

    make implicit animation clip by name merge explicit by adding a component for it

---
+++ UnityGLTF/Assets/UnityGLTF/Runtime/Scripts/Animation/GLTFAnimationExportSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace UnityGLTF
+{
+	public class GLTFAnimationExportSettings : MonoBehaviour
+	{
+		[Tooltip("If enabled animator states with the same name " +
+		         "or clips that have the same name will be merged into one animation clip on export")]
+		public bool mergeClipsWithMatchingNames = true;
+	}
+}
