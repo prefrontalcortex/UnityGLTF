4e76b669875710987378bf3e6ae085f7c452b7dd
pfcDorn <dorn@prefrontalcortex.de>
2023-08-24 17:03:44 +0200

    mutable material fixing draft

--- Editor/Scripts/ShaderGraph/MaterialEditorBridge.cs
+++ Editor/Scripts/ShaderGraph/MaterialEditorBridge.cs
@@ -23,10 +23,23 @@ namespace UnityGLTF
 			// var mainAsset = AssetDatabase.LoadMainAssetAtPath(assetPath);
 
 			// Transform[] rootTransforms = null;
-			var exporter = new GLTFSceneExporter((Transform[]) null, new ExportOptions());
+			var glftSettingsCopy =  ScriptableObject.Instantiate(GLTFSettings.GetOrCreateSettings());
+			glftSettingsCopy.ExportFullPath = true;
+			glftSettingsCopy.TryExportTexturesFromDisk = true;
+			var exportSettings = new ExportOptions(glftSettingsCopy);
+			exportSettings.TexturePathRetriever += texture =>
+			{
+				return AssetDatabase.GetAssetPath(texture);
+			};
+			var exporter = new GLTFSceneExporter((Transform[]) null, exportSettings);
 			// load all materials from mainAsset
 			var importer = AssetImporter.GetAtPath(assetPath) as GLTFImporter;
 			if (!importer) return;
+
+			var path = Path.GetDirectoryName(assetPath);
+			var name = Path.GetFileName(assetPath);
+
+
 
 			// var allObjects = AssetDatabase.LoadAllAssetsAtPath(assetPath);
 			foreach (var obj in importer.m_Materials)
@@ -36,46 +49,35 @@ namespace UnityGLTF
 					// TODO warn that there are extra objects we can't store right now
 					continue;
 				}
-
 				exporter.ExportMaterial(mat);
 			}
 
-			var path = Path.GetDirectoryName(assetPath);
-			var name = Path.GetFileName(assetPath);
+			var images = exporter.GetRoot().Images;
+			foreach (var image in images)
+				if (!string.IsNullOrEmpty(image.Uri))
+					image.Uri  = Path.GetRelativePath(  path, image.Uri);
 
 			// Save file and make sure we reimport it
 			exporter.SaveGLTFandBin(path, name, false);
-			AssetDatabase.ImportAsset(path);
+			ScriptableObject.DestroyImmediate(glftSettingsCopy);
+			AssetDatabase.ImportAsset(assetPath);
+
+			importer = AssetImporter.GetAtPath(assetPath) as GLTFImporter;
+			if (!importer) return;
+
 
 			// add texture remaps, so that the textures we just exported with stay valid.
 			// We want to always have default paths in the file, and remapping is done on the Unity side to a project specific path,
 			// to avoid breaking references all the time when paths change.
 			var exportedTextures = exporter.GetRoot().Textures;
-			var importedTextures = importer.m_Textures;
-			// If these don't match, we could only try by name... not ideal.
-			// They may not match due to different sampler settings etc.
-			if (exportedTextures.Count == importedTextures.Length)
+
+			for (int i = 0; i < exportedTextures.Count; i++)
 			{
-				for (int i = 0; i < exportedTextures.Count; i++)
-				{
-					var exported = exportedTextures[i];
-					var imported = importedTextures[i];
-					// TODO could we also use the imported texture definitions instead of the actual imported textures?
-					// Then we wouldn't need to create mock textures on import for all missing/remapped ones.
-					if (exported.Name != imported.name)
-					{
-						Debug.LogWarning($"Texture name mismatch: {exported.Name} != {imported.name}");
-					}
-
-					var sourceTextureForExportedTexture = exporter.GetSourceTextureForExportedTexture(exported);
-
-					// we should not remap if that is actually the same texture
-					if (imported == sourceTextureForExportedTexture) continue;
-
-					importer.AddRemap(new AssetImporter.SourceAssetIdentifier(imported), sourceTextureForExportedTexture);
-				}
-				importer.SaveAndReimport();
+				var exported = exportedTextures[i];
+				var sourceTextureForExportedTexture = exporter.GetSourceTextureForExportedTexture(exported);
+				importer.AddRemap(new AssetImporter.SourceAssetIdentifier(typeof(Texture2D), exported.Name), sourceTextureForExportedTexture);
 			}
+			importer.SaveAndReimport();
 
 			// TODO we should get rid of this, but without it currently the inspector doesn't repaint
 			// after importing a changed material, which can be confusing. Could be caching inside PBRGraphGUI
